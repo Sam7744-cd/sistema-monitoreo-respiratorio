1b49aeb738197d09e71423894334ef21835c7efe
Sam7744-cd <andreamoralesmv@gmail.com>
2025-12-02 20:05:58 -0500

    sin token

--- src/routes/medicionTiempoRealRoutes.js
+++ src/routes/medicionTiempoRealRoutes.js
@@ -2,36 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  recibirMedicion,     // ESP32 envía mediciones
-  obtenerActual        // App obtiene última medición
+  recibirMedicion,
+  obtenerActual
 } = require("../controllers/medicionTiempoRealController");
 
 const {
-  setPacienteActual    // App selecciona paciente actual
+  setPacienteActual
 } = require("../controllers/tiempoRealSeleccionController");
 
-// Middlewares
-const authMiddleware = require("../middleware/auth");
-const roleMiddleware = require("../middleware/roles");
-
-// Todas las rutas requieren autenticación
-router.use(authMiddleware);
-
-// ESP32 envía datos en tiempo real
+//     1) ESP32 ENVÍA DATOS --> NO debe requerir token
 router.post("/", recibirMedicion);
 
-// App selecciona paciente para monitoreo
-// Médico y Familiar
+//     2) APP SELECCIONA PACIENTE 
+router.post("/set-paciente", setPacienteActual);
 
-router.post(
-  "/set-paciente",
-  roleMiddleware(["medico", "familiar"]),
-  setPacienteActual
-);
 
-// Obtener última medición del paciente seleccionado
-// (lo pueden ver tanto médico como familiar)
+//     3) OBTENER ÚLTIMA MEDICIÓN 
 router.get("/actual", obtenerActual);
 
 module.exports = router;
-
